3e16d8dce3ced35b8eb077b608efe96389e10d19
NikolaiRozhkov <agent-nik126@mail.ru>
2021-10-23 13:05:19 +0300

    Last

--- menu.js
+++ menu.js
@@ -1,13 +1,11 @@
-window.addEventListener(`resize`, event => {
-    if (document.documentElement.clientWidth < 630) {
-        console.log(document.documentElement.clientWidth);
-        document.body.style.zoom="65%";
-    }
-    else {
-        document.body.style.zoom="100%";
-    };
-  }, false);
-  
+if (document.documentElement.clientWidth < 630) {
+    console.log(document.documentElement.clientWidth);
+    document.body.style.zoom="65%";
+}
+else {
+    document.body.style.zoom="100%";
+};
+
 $(document).ready(function() {
     $('.header-burger').click(function(event) {
         $('.header-burger,.header-menu').toggleClass('active');
