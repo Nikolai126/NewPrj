5e350190d44ddf23854108f4a8ba83eb72e16ed6
NikolaiRozhkov <agent-nik126@mail.ru>
2021-10-23 12:22:51 +0300

    widthMain

--- menu.js
+++ menu.js
@@ -1,4 +1,13 @@
-document.body.style.zoom="65%"
+window.addEventListener(`resize`, event => {
+    if (document.documentElement.clientWidth < 630) {
+        console.log(document.documentElement.clientWidth);
+        document.body.style.zoom="65%";
+    }
+    else {
+        document.body.style.zoom="100%";
+    };
+  }, false);
+  
 $(document).ready(function() {
     $('.header-burger').click(function(event) {
         $('.header-burger,.header-menu').toggleClass('active');
